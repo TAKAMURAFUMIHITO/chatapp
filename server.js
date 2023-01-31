b8057d9fdc62013360c8cdcbbe9dff72cda50c34
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-01-31 18:23:41 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
+
+server.listen(8000, () => {
+  console.log('listening on 8000');
+});
