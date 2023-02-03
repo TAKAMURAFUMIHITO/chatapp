7a049a6f44c1b6aa6ca76f2c622b587e46f0e3ff
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-02-03 12:29:57 +0900

    add chat function

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ const http = require("http");
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
+app.use(express.static('frontend'));
+
+/*
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__frontendDir + "/index.html");
 })
+*/
+
+io.on('connection', (socket) => {
+  console.log('ユーザーが接続しました。');
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
 
 server.listen(8000, () => {
   console.log('listening on 8000');
